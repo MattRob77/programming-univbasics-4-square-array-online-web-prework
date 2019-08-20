d5f0d2411b45cdd43b2b9b7f06e9c299b68df0bf
Matthew Robinson <matt_robinson1@yahoo.com>
2019-08-20 16:46:29 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,8 @@
 def square_array(numbers) 
   counter = 0 
   new_array =[]
-  while numbers[counter] do 
-    numbers[counter] ** 2 << 
+  while numbers[counter] do
+   new_array << numbers[counter] ** 2 
     
   
 end
